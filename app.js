eaf417bb0028eb39adea9fe117b4aee77e64425e
Christoph Friedrich <christoph@friedrich-whv.de>
2017-03-15 16:48:04 +0100

    add service's feature layers to map

--- app.js
+++ app.js
@@ -82,11 +82,102 @@ function addMaps(data) {
 }
 
 function addFeatures(data) {
-    // TODO
-    console.log("FEATURES");
-    data.features.forEach(function(layer) {
-       console.log(layer.title); 
-    });
+    // loop through feature sets that the service provides
+    data.features.forEach(function(layer) {        
+        // defaults
+        var params = '';
+        var options = {};
+        var cluster = false;
+        
+        switch(layer.title)
+        {
+            case 'warning-shapes-fine':
+                params = '?bbox=47.270362,5.876914,55.044381,15.037507'; // all of Germany
+                // in theory it should be possible to use the CRS's bbox for all of Germany :thinkingfaceemoji:
+                //params += '?crs=EPSG:3857&bbox=' + [data.crs.southBoundLatitude, data.crs.westBoundLongitude, data.crs.northBoundLatitude, data.crs.eastBoundLongitude].join(',');
+                options = {
+                    style: function (feature) {
+                        return {fillOpacity: 0.1};  // very subtle fill
+                    },
+                    onEachFeature: function (feature, layer) {
+                        // compose popup
+                        layer.bindPopup(Object.keys(feature.properties).map((e)=> '<strong>' + e + ':</strong> ' + feature.properties[e]).join("<br>\n"));
+                    }
+                };
+                break;
+                
+            case 'Heavy Metal Samples':
+                // we collect all the timestamps in an array so that an external control could manage which to show when
+                //zeitpunkte = [];
+                params = '?bbox=13.765706,13.726043,51.086888,51.003874';
+                options = {
+                    style: function (feature) {
+                        // color marker circles green or read according to whether Cd_SUMM exceeds 0.1 µg/g
+                        return (parseFloat(feature.properties.Cd_SUMM___micro_g_per_g_) < 0.1) ? {color:'green'} : {color:'red'};
+                    },
+                    pointToLayer: function(feature, latlng) {
+                        // use this to receive marker circles (which can be styled with the function above) instead of simple markers (which cannot)
+                        return new L.CircleMarker(latlng, {radius: 10, fillOpacity: 0.85});
+                    },
+                    filter: function (feature, layer) {
+                        // only show values dating from the 27th of July 2012
+                        return feature.properties.Timestamp.indexOf('2012-07-27') > -1;
+                    },
+                    onEachFeature: function (feature, layer) {
+                        // compose popup (this time using a table instead of simple "key: value")
+                        var html = Object.keys(feature.properties).map((e)=>'<td>' + e.replace('___micro_g_per_g_', '').replace('_', ' ')+'</td><td>' + feature.properties[e] + '</td>').join('</tr><tr>');
+                        html = '<table border="1" style="border-collapse:collapse; white-space:nowrap"><tr>'+html+'</tr></table>';
+                        layer.bindPopup(html);
+                        //zeitpunkte.push(feature.properties.Timestamp);
+                    }
+                };
+                break;
+            
+            case 'emmission-simulation-results':
+                L.rectangle([[51.086888,13.825706],[51.003874,13.765706]], {color: "red", weight: 5, fill: false}).addTo(map);  // bbox
+                params = '?bbox=51.003874,13.765706,51.086888,13.825706';
+                // no options/popup
+                cluster = true;  // cluster these markers because there are MANY (like 20,000+)
+                break;
+                
+            case 'urban-atlas-2006-dresden':
+                L.rectangle([[51.076888,13.706043],[51.086888,13.726043]], {color: "green", weight: 5, fill: false}).addTo(map);  // bbox
+                params = '?bbox=51.076888,13.706043,51.086888,13.726043';
+                options = {
+                    style: function (feature) {
+                        // color each feature randomly
+                        return {
+                            color: "#000000".replace(/0/g, ()=>(~~(Math.random()*16)).toString(16)),
+                            weight:2,
+                            fillOpacity:0.4
+                        };
+                    },
+                    filter: function(feature, layer) {
+                        // Hide features that are huge
+                        return parseFloat(feature.properties.shape_area) < 1000000;
+                    },
+                    onEachFeature: function (feature, layer) {
+                        // fancy popup once more
+                        var html = Object.keys(feature.properties).map((e)=>'<td>'+e+'</td><td>' + feature.properties[e] + '</td>').join('</tr><tr>');
+                        html = '<table border="1" style="border-collapse:collapse"><tr>'+html+'</tr></table>';
+                        layer.bindPopup(html);
+                    }
+                };
+                break;
+        } // end switch
+        
+        // take the configured variables, get the data...
+        $.get(layer.href + params, function(geojsonresponse) {
+            // parse it
+            var newlayer = L.geoJson(geojsonresponse, options);
+            // cluster the markers if configured to do so
+            if(cluster) { newlayer = L.markerClusterGroup().addLayer(newlayer); }
+            // add to map
+            newlayer.addTo(map);
+            // send warning-shapes-fine layer to back because otherwise it blocks other objects from being clicked to see their popups
+            if(layer.title == 'warning-shapes-fine') { newlayer.bringToBack(); }
+        });        
+    }); 
 }
 
 $(document).ready(function() {
