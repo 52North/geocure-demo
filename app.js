cabbfca5bc90cf0fdf12dd4a3e54e909f5041dee
Trzaska <Niklas.Trzaska@uni-muenster.de>
2017-10-12 11:45:34 +0200

    Event handling for onle layer works, but not for all...

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ function initMap() {
         // crs: L.CRS.EPSG4326   // changes Leaflet's CRS to EPSG:4326, might be useful for some purposes, but breaks OSM tiles
     });
 
+
+
     // add layer control to map
     layercontrol = L.control.layers().addTo(map).expand();
 
@@ -36,7 +38,7 @@ function initMap() {
     */
 
     // "Clear" "basemap" in case one of the map's overlays is used as the actual basemap (e.g. urban-atlas-2006-dresden)
-    layercontrol.addBaseLayer(L.rectangle([[-90,-180],[90,180]], {fill: false}), '[basemaps] none');
+    // layercontrol.addBaseLayer(L.rectangle([[-90,-180],[90,180]], {fill: false}), '[basemaps] none');
 
     // example marker
     L.marker([51.049259, 13.73836]).addTo(map).bindPopup('Dresden city centre').openPopup();
@@ -49,7 +51,7 @@ function getServices() {
             $("#serviceslist").append($('<li>', { id: service.id, text: service.label, title: service.description }).data("href", service.href).click(initService));
         });
     });
-}
+};
 
 function initService() {
     // get maps and features and handle them
@@ -64,13 +66,16 @@ function addMaps(data) {
     var overlays = {};
 
     // Show Dresden bbox (all maps except warning-shapes-fine are clipped to this bbox)
-    L.rectangle([[50.990421,13.63266],[51.105678,13.83316]], {color: "#ff7800", weight: 5, fill: false}).addTo(map);
+  //  L.rectangle([[50.990421,13.63266],[51.105678,13.83316]], {color: "#ff7800", weight: 5, fill: false}).addTo(map);
+  //  L.rectangle([[50.990421,13.63266],[51.105678,13.83316]], {color: "#ff7800", fill: false}).addTo(map);
+
 
     // loop through layers that the service provides
     data.layers.forEach(function(layer) {
         var params = '';
         var imageBounds;
 
+        console.log(JSON.stringify(layer));
         switch(layer.title) {
             case 'warning-shapes-fine':   // all of Germany
                 params = '&crs=EPSG:3857';  // set crs to EPSG:3857 so that the returned CRS object will have the same CRS as Leaflet
@@ -84,7 +89,12 @@ function addMaps(data) {
 
         // add map to overlays collection
         var imageUrl = layer.href + params;
-        var newlayer = L.imageOverlay(imageUrl, imageBounds, {opacity:0.5});
+        var newlayer = L.imageOverlay(imageUrl, imageBounds, {opacity:0.5, interactive: true});
+        // console.log(newlayer);
+        // newlayer.on('click', function(d) {alert('I have been clicked ')});
+        // newlayer.addTo(map);
+        newlayer.on('click', function(){alert('hey')});
+        console.log('Added event handler for ' + layer.title);
         overlays[layer.title] = newlayer;
         layercontrol.addOverlay(newlayer, '[maps] ' + layer.title);
     });
@@ -92,6 +102,9 @@ function addMaps(data) {
     // add opacity control (sets opacity of all layers together [no individual opacity])
     L.control.layerOpacity({layers: overlays}).addTo(map);
 }
+
+
+
 
 function addFeatures(data) {
     // loop through feature sets that the service provides
@@ -146,14 +159,14 @@ function addFeatures(data) {
                 break;
 
             case 'emission-simulation': // formerly known as 'emmission-simulation-results'
-                L.rectangle([[51.026888,13.825706],[51.003874,13.765706]], {color: "red", weight: 5, fill: false}).addTo(map);  // bbox
+                // L.rectangle([[51.026888,13.825706],[51.003874,13.765706]], {color: "red", weight: 5, fill: false}).addTo(map);  // bbox
                 params = '?bbox=51.003874,13.765706,51.026888,13.825706';
                 // no options/popup
                 cluster = true;  // cluster these markers because there are MANY (like 20,000+)
                 break;
 
             case 'urban-atlas-2006-dresden':
-                L.rectangle([[51.076888,13.706043],[51.086888,13.726043]], {color: "green", weight: 5, fill: false}).addTo(map);  // bbox
+                // L.rectangle([[51.076888,13.706043],[51.086888,13.726043]], {color: "green", weight: 5, fill: false}).addTo(map);  // bbox
                 params = '?bbox=51.076888,13.706043,51.086888,13.726043';
                 options = {
                     style: function (feature) {
@@ -178,7 +191,7 @@ function addFeatures(data) {
                 break;
 
             case 'street-cleaning':
-                L.rectangle([[51.030888,13.716043],[51.040888,13.736043]], {color: "purple", weight: 5, fill: false}).addTo(map);  // bbox
+                // L.rectangle([[51.030888,13.716043],[51.040888,13.736043]], {color: "purple", weight: 5, fill: false}).addTo(map);  // bbox
                 params = '?bbox=51.030888,13.716043,51.040888,13.736043';
                 options = {
                     style: function (feature) {
