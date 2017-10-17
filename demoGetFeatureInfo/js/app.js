6a7c978897fab8dd4359f354d1df40759bad9704
Trzaska <Niklas.Trzaska@uni-muenster.de>
2017-10-17 17:51:12 +0200

    Cleaned up code.

--- demoGetFeatureInfo/js/app.js
+++ demoGetFeatureInfo/js/app.js
@@ -3,46 +3,9 @@ const host = 'http://127.0.0.1:8002/geocure';
 
 window.onload = function() {
 
-  // Getting the width and height of the canvas. Used for requesting the image and the feature info.
-  let canvasheight = $('#mapCanvas').attr('height');
-  let canvaswidth = $('#mapCanvas').attr('width');
-
-  let dataURL = host + '/services/colabis-geoserver/map/render?layer=ckan:_53fbae20_e2fb_4fd1_b5d6_c798e11b96d1&width=' + canvaswidth + '&height=' + canvasheight;
-
-  getAvailableMaps().then(addMapsToSelectOptions).then(addEventlistenerToSelect)
-
-  loadCanvas(dataURL);
-
-  function loadCanvas(dataURL) {
-    var canvas = document.getElementById('mapCanvas');
-    var context = canvas.getContext('2d');
-
-    // load image from data url
-    var imageObj = new Image();
-    imageObj.onload = function() {
-      context.drawImage(this, 0, 0);
-    };
-    imageObj.src = dataURL;
-  }
-
-
-
-  $('#mapCanvas').on('click', function(event) {
-    console.log('Canvas click');
-    var x = event.offsetX;
-    var y = event.offsetY;
-
-    let size = '&height=' + $('#mapCanvas').attr('height') + '&width=' + $('#mapCanvas').attr('width');
-    let requestUrl = host + '/services/colabis-geoserver/map/info?layer=ckan:_53fbae20_e2fb_4fd1_b5d6_c798e11b96d1&x=' + x + '&y=' + y + size;
-    console.log("meta");
-    console.log(requestUrl);
-    $.get(requestUrl, function(res) {
-      $('#json').empty();
-      $('#json').append(JSON.stringify(res.features[0].properties.typed_name, null, 2));
-      $('#myModal').modal();
-    });
-
-  });
+  getAvailableMaps()
+    .then(addMapsToSelectOptions)
+    .then(addEventlistenerToSelect);
 }
 
 
@@ -74,7 +37,6 @@ function addMapsToSelectOptions(availaleMaps) {
         }
         else {
           let option = $('<option>').text(map[i].title).attr('href', map[i].href);
-          console.log(option);
           $('#availableMaps').append(option).selectpicker('refresh');
         }
       }
@@ -89,10 +51,12 @@ function addEventlistenerToSelect() {
   return new Promise((resolve, reject) => {
     try {
       $('#availableMaps').change(function(){
-        let linkToMap = $( "#availableMaps option:selected" ).attr('href');
-        reloadMapOnCanvas(linkToMap).then(updateOnClickCanvasEvent);
-
-      })
+        let linkToMap = $("#availableMaps option:selected").attr('href');
+        if(linkToMap){
+          reloadMapOnCanvas(linkToMap).then(updateOnClickCanvasEvent).then(resolve);
+        }
+        resolve()
+      });
     }
     catch(error) {
       reject(error);
@@ -104,18 +68,19 @@ function addEventlistenerToSelect() {
 function reloadMapOnCanvas(linkToMap) {
   return new Promise((resolve, reject) => {
     try {
-      let size = '&height=' + $('#mapCanvas').attr('height') + '&width=' + $('#mapCanvas').attr('width');
-      let imageRequestURL = linkToMap + size;
-      var canvas =document.getElementById('mapCanvas');
-      var context = canvas.getContext('2d');
 
-      // load image from data url
-      var imageObj = new Image();
-      imageObj.onload = function() {
-        context.drawImage(this, 0, 0);
-        resolve({url: linkToMap, size: size});
-      };
-      imageObj.src = imageRequestURL;
+        let size = '&height=' + $('#mapCanvas').attr('height') + '&width=' + $('#mapCanvas').attr('width');
+        let imageRequestURL = linkToMap + size;
+        var canvas =document.getElementById('mapCanvas');
+        var context = canvas.getContext('2d');
+
+        // load image from data url
+        var imageObj = new Image();
+        imageObj.onload = function() {
+          context.drawImage(this, 0, 0);
+          resolve({url: linkToMap, size: size});
+        };
+        imageObj.src = imageRequestURL;
 
     }
     catch(error) {
@@ -127,7 +92,7 @@ function reloadMapOnCanvas(linkToMap) {
 function updateOnClickCanvasEvent(imageRequestURL) {
   return new Promise((resolve, reject) => {
     try {
-      console.log(imageRequestURL)
+
       const basisInfoURl = '/services/colabis-geoserver/map/info?';
       let currentLayer = imageRequestURL.url.match(/layer=.*[^\/]/);
       let size = imageRequestURL.size;
@@ -136,13 +101,12 @@ function updateOnClickCanvasEvent(imageRequestURL) {
       $('#mapCanvas').off() // Remove all eventlisteners, added with .on
 
       $('#mapCanvas').on('click', function(event) {
-        console.log('Canvas click');
+
         var x = event.offsetX;
         var y = event.offsetY;
 
         let requestUrl = host + basisInfoURl + currentLayer + size + '&x=' + x + '&y=' + y;
-        console.log("meta");
-        console.log(requestUrl);
+
         $.get(requestUrl, function(res) {
           $('#json').empty();
           $('#json').append(JSON.stringify(res, null, 2));
