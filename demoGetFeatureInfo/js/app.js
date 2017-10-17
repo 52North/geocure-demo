37ddddb3c55d99a644b353dc48bd11923158996f
Trzaska <Niklas.Trzaska@uni-muenster.de>
2017-10-17 11:10:16 +0200

    this was it is working... but not good style...

--- demoGetFeatureInfo/js/app.js
+++ demoGetFeatureInfo/js/app.js
@@ -11,9 +11,9 @@ window.onload = function() {
       context.drawImage(this, 0, 0);
     };
     console.log("nod adding image src");
-    let imageURL = 'https://www.google.de/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiEh6GWpPfWAhUCblAKHSjYBhUQjRwIBw&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FPortable_Network_Graphics&psig=AOvVaw3VSmvhlFDlTLI4n9aSGAXW&ust=1508316814380003'
+    let imageURL = 'https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png'
     let url = 'http://127.0.0.1:8002/geocure/services/colabis-geoserver/map/render?layer=ckan:_53fbae20_e2fb_4fd1_b5d6_c798e11b96d1';
-    imageObj.src = imageURL;
+    imageObj.src = url;
 
   }
 
