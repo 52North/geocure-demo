65a88b8d75663eb6d2fae43d1050be18d44d007e
Trzaska <Niklas.Trzaska@uni-muenster.de>
2017-10-17 11:53:42 +0200

    refactoring

--- demoGetFeatureInfo/js/app.js
+++ demoGetFeatureInfo/js/app.js
@@ -1,5 +1,7 @@
 window.onload = function() {
-  loadCanvas();
+  let dataURL = 'http://127.0.0.1:8002/geocure/services/colabis-geoserver/map/render?layer=ckan:_53fbae20_e2fb_4fd1_b5d6_c798e11b96d1';
+
+  loadCanvas(dataURL);
 
   function loadCanvas(dataURL) {
     var canvas = document.getElementById('mapCanvas');
@@ -11,9 +13,7 @@ window.onload = function() {
       context.drawImage(this, 0, 0);
     };
     console.log("nod adding image src");
-    let imageURL = 'https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png'
-    let url = 'http://127.0.0.1:8002/geocure/services/colabis-geoserver/map/render?layer=ckan:_53fbae20_e2fb_4fd1_b5d6_c798e11b96d1';
-    imageObj.src = url;
+    imageObj.src = dataURL;
 
   }
 
@@ -21,7 +21,7 @@ window.onload = function() {
     console.log("adding click event");
     var x = event.offsetX;
     var y = event.offsetY;
-
+    
     let requestUrl = 'http://127.0.0.1:8002/geocure/services/colabis-geoserver/map/info?layer=ckan:_53fbae20_e2fb_4fd1_b5d6_c798e11b96d1&x=' + x + '&y=' + y;
 
     $.get(requestUrl, function(res) {
